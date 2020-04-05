16ed28ed653253b9ffaf41437c3d3338fb948f1a
nathanclough <nathan.clough@ttu.edu>
2020-04-05 12:33:11 -0500

    Added button icon in bottom left

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import {Icon} from 'react-native-elements' 
 
 export default function Home(props) {
 
     return (
-        <View
-            style={{
-                flex: 1,
-                backgroundColor: '#fff',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}
-        >
-            <Text>This is the Home screen</Text>
-        </View>
+<View style={{flex:1,justifyContent:"center",backgroundColor:"#fff", alignItems:"stretch"}}>
+    
+    
+    
+    <View style={{flex:1,backgroundColor:"white",alignItems: 'flex-end',justifyContent: 'flex-end'}}>
+        <Icon
+            name ='plus'
+            type ='simple-line-icon'
+            color ='#00BDAA'
+            reverse
+
+            />
+    </View>
+</View>
     );
 
 };
