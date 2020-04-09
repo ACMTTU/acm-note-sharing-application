3b4c2f727f79b5d5619dda1c2ba12cfb20bc847c
nathanclough <nathan.clough@ttu.edu>
2020-04-09 16:52:51 -0500

    Added in classroom component with its own styling and switched the switched from styling the view to the button

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
 import { View, Text, StyleSheet, } from 'react-native'
 import {Button } from 'react-native-elements'
-/* import {Icon} from 'react-native-elements'  */
+import {Card} from 'react-native-elements'   
+import Classroom from './Classroom'
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 export default function Home(props) {
 
+
     return (
-    <View style={styles.container}>
+    <View>
+    <Classroom 
+        style = {styles.classroomStyle}  
+        courseName="100" 
+        title="Math" 
+        semester="Spring" 
+        professor="John"  
+    />  
+
+    <Classroom 
+        style = {styles.classroomStyle}  
+        courseName="100" 
+        title="Math" 
+        semester="Spring" 
+        professor="John"  
+    />  
+    
         <Button
+            style ={styles.ButtonStyle} 
             type = "clear" 
             icon={<Icon
                 name="plus"
                 size={60}
                 color="#00BDAA"
                 />
-            }
+                }
             onClick={() => {}}    
         />
                 
@@ -25,12 +44,18 @@ export default function Home(props) {
 
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    ButtonStyle: {
         justifyContent: "flex-end",
-        alignItems: "flex-end",
+        alignItems: "flex-end", 
         paddingRight: 25,
-        paddingBottom: 20
+        paddingBottom: 20,
+
+    },
+    classroomStyle: {
+        flex: 1,        
+        alignItems: "center",
+        justifyContent: "center",
+
     },
   
   
