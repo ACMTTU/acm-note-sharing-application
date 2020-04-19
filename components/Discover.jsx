e01995f2c17434beea92ad63ab7788e544670d56
jacksonkays <55301666+jacksonkays@users.noreply.github.com>
2020-04-19 10:06:36 -0500

    Worked on adding a search bar

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { SearchBar } from 'react-native-elements';
 
 export default function Discover(props){
 
-    return (
+    export default class SearchBar extends React.Component {
+        state = {
+            search:'',
+        };
+        updateSearch = search => {
+            this.setState({search});
+        };
+        render() {
+            const {search} = this.state;
+            return (
+                <View
+                    style={{
+                        top: 0,
+                        flex: 1,
+                        justifyContent: 'center',
+                    }}
+                >
+                <SearchBar
+                    placeholder = "Search notes here..."
+                    onChangeText = {this.updateSearch}
+                    value = {search}
+                />
+                </View>
+            )
+        };
+    }
+    return ( 
         <View
             style={{
                 flex: 1,
@@ -12,7 +39,7 @@ export default function Discover(props){
                 justifyContent: 'center',
             }}
         >
-            <Text>This is Jack editing</Text>
+            <Text>This is Jack editing and testing </Text>
         </View>
     )
 };
