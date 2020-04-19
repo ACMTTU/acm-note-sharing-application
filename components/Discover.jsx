7c47c769987d71b89a39b0dde75733d13ba81384
jacksonkays <55301666+jacksonkays@users.noreply.github.com>
2020-04-19 10:26:56 -0500

    debugging

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -3,40 +3,16 @@ import { View, Text } from 'react-native'
 import { SearchBar } from 'react-native-elements';
 
 export default function Discover(props){
-        state = {
-            search:'',
-        };
-        updateSearch = search => {
-            this.setState({search});
-        };
-        render() {
-            const {search} = this.state;
-            return (
-                <View
-                    style={{
-                        top: 0,
-                        flex: 1,
-                        justifyContent: 'center',
-                    }}
-                >
-                <SearchBar
-                    placeholder = "Search notes here..."
-                    onChangeText = {this.updateSearch}
-                    value = {search}
-                />
-                </View>
-            )
-        };
     return ( 
-        <View
-            style={{
+        <View>
+            <Text
+            style = {{
                 flex: 1,
-                backgroundColor: '#fff',
+                backgroundColor: "#fff",
                 alignItems: 'center',
                 justifyContent: 'center',
             }}
-        >
-            <Text>This is Jack editing and testing </Text>
+            >This is Jack editing and testing </Text>
         </View>
     )
 };
